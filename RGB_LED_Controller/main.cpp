93ce74cbbc16c382b723f56e17d1df2842f6ce7e
Sergey <sergeyg@impekable.com>
2016-06-13 14:37:47 -0700

    git repo created for empty cpp project

---
+++ RGB_LED_Controller/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * RGB_LED_Controller.cpp
+ *
+ * Created: 6/13/2016 2:32:57 PM
+ * Author : Alex
+ */ 
+
+#include <avr/io.h>
+#include "compiler.h"
+
+int main(void)
+{
+	
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
